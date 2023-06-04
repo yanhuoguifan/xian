aba0f72927c5a76363f859d88511752018e4b181
lixingyang <1127955419@qq.com>
2023-06-04 12:09:43 +0800

    编写了重新刷新tlb的机制
    Signed-off-by: lixingyang <1127955419@qq.com>

---
+++ arch/x86/include/asm/system.h
@@ -0,0 +1,25 @@
+#ifndef _ASM_X86_SYSTEM_H
+#define _ASM_X86_SYSTEM_H
+
+/*
+ * Volatile isn't enough to prevent the compiler from reordering the
+ * read/write functions for the control registers and messing everything up.
+ * A memory clobber would solve the problem, but would prevent reordering of
+ * all loads stores around it, which can hurt performance. Solution is to
+ * use a variable and mimic reads and writes to it to enforce serialization
+ */
+static unsigned long __force_order;
+
+static inline unsigned long native_read_cr3(void)
+{
+	unsigned long val;
+	asm volatile("mov %%cr3,%0\n\t" : "=r" (val), "=m" (__force_order));
+	return val;
+}
+
+static inline void native_write_cr3(unsigned long val)
+{
+	asm volatile("mov %0,%%cr3": : "r" (val), "m" (__force_order));
+}
+
+#endif /* _ASM_X86_SYSTEM_H */
--- arch/x86/include/asm/tlbflush.h
+++ arch/x86/include/asm/tlbflush.h
@@ -1,7 +1,15 @@
 #ifndef _ASM_X86_TLBFLUSH_H
 #define _ASM_X86_TLBFLUSH_H
 
+#include <asm/system.h>
+
+#define __flush_tlb() __native_flush_tlb()
 #define __flush_tlb_global() __native_flush_tlb_global()
+
+static inline void __native_flush_tlb(void)
+{
+	native_write_cr3(native_read_cr3());
+}
 
 static inline void __native_flush_tlb_global(void)
 {
@@ -9,7 +17,10 @@ static inline void __native_flush_tlb_global(void)
 
 static inline void __flush_tlb_all(void)
 {
+	if (0)
 		__flush_tlb_global();
+	else
+		__flush_tlb();
 }
 
 #endif /* _ASM_X86_TLBFLUSH_H */
